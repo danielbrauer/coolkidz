ed569b0371dae46a4a95a8a0372d311681379391
Daniel Brauer <daniel@silverbackgames.com>
2014-11-25 01:04:42 -0400

    Ocean increases drag on impact

---
+++ Assets/Scripts/IncreaseDrag.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseDrag : MonoBehaviour {
+	
+	[SerializeField] float drag = 20f;
+	[SerializeField] float angularDrag = 20f;
+	
+	void OnTriggerEnter(Collider other) {
+		var rigidbody = other.attachedRigidbody;
+		if (rigidbody) {
+			rigidbody.drag = drag;
+			rigidbody.angularDrag = angularDrag;
+		}
+	}
+}
