7737e1f50f5e420241b075637e2d9848f914eba4
Daniel Brauer <daniel@silverbackgames.com>
2014-11-24 23:55:57 -0400

    Control cars with AS and JK Cars can't turn left or right Cars are blocked from reversing at the beginning Prefabbed the cars

---
+++ Assets/SampleAssets/Vehicles/Car/Scripts/CarDragRaceControl.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarDragRaceControl : MonoBehaviour
+    {
+		[SerializeField] private string inputAxis;
+        private CarController car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+			// Vector3 trackPosition = track.InverseTransformPoint(transform.position);
+			// float h = -trackPosition.x*correctionStrength;
+            // pass the input to the car!
+            // float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis(inputAxis);
+            car.Move(0f, v);
+        }
+    }
+}
